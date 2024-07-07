365ae6fca8d64d6aa79b2156ac6acccabafabec5
legendajar <harshsolanki9898@gmail.com>
2024-07-07 21:27:34 +0530

    chore: Add real-time tracking functionality with geolocation and socket.io

---
+++ public/js/script.js
@@ -0,0 +1,45 @@
+const socket = io(); // This wiil send connection request to backend
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        socket.emit("send-location", { latitude, longitude})
+    }, 
+    (error) => {
+        console.error(error);
+    },
+    {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    }
+);
+}
+
+const map = L.map("map").setView([0,0], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "Shreyians Coding School",
+}).addTo(map);
+
+
+const markers = {};
+
+socket.on("recieve-location", (data) => {
+    const {id, latitude, longitude} = data;
+    map.setView([latitude, longitude], 16);
+    if(markers[id]){
+        markers[id].setLatLng([latitude, longitude]);
+    }
+    else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
+
+
+socket.on("user-disconnected", (id) => {
+    if (markers[id]) {
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
